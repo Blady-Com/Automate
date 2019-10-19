14a01d747c2c9749a3cb271268d7ff943a40ac72
Blady <blady.net@orange.fr>
2019-10-19 19:05:51 +0200

    Change Global specification in order to avoid: arbmgr.ads:46:07: medium: "IdAuto.ListeState" must be a global In_Out of "Balance", in instantiation at insrc.ads:51 arbmgr.ads:48:07: medium: "IdAuto.ListeState" must be a global In_Out of "Recherche", in instantiation at insrc.ads:51 arbmgr.ads:50:07: medium: "IdAuto.ListeState" must be a global In_Out of "RetournePremier", in instantiation at insrc.ads:51

--- src/arbmgr.ads
+++ src/arbmgr.ads
@@ -43,11 +43,11 @@ is
    procedure Ajoute (Clef : TClef; Element : TElement) with
       Global => (In_Out => ArbMgrState);
    procedure Balance with
-      Global => (In_Out => ArbMgrState, Output => ListeState);
+      Global => (In_Out => (ArbMgrState, ListeState));
    procedure Recherche (Clef : TClef; Element : out TElement) with
-      Global => (In_Out => ArbMgrState, Output => ListeState);
+      Global => (In_Out => (ArbMgrState, ListeState));
    procedure RetournePremier (Element : out TElement) with
-      Global => (In_Out => ArbMgrState, Output => (ListeState, CurElmtState));
+      Global => (In_Out => (ArbMgrState, ListeState), Output => CurElmtState);
    procedure RetourneSuivant (Element : out TElement) with
       Global => (In_Out => CurElmtState);
    procedure Detruit with
